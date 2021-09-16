f3d84a3f1b837d8186637eb79b4527043e853324
Elizabeth Moore <m.elizabeth.moore73@gmail.com>
2021-09-16 10:42:30 -0400

    First commit, writing data cleaning script for analysis in R

---
+++ R scripts/artificial-nectar_grwth-rate_cleaning-script.R
@@ -0,0 +1,45 @@
+#Artificial Nectar growth rate experiment
+#cleaning script
+
+
+#load libraries
+library(readr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+
+#load data
+angr <- read_csv("data files/artificial-nectar_grwth-rate-data_raw.csv")
+View(angr)
+
+angr_key <- read_csv("data files/artificial-nectar_grwth-rate_key.csv")
+View(angr_key)
+
+
+theme_set(theme_classic())
+
+#Matching well IDs to strain, treatment and replicate IDs---------------
+
+#Plot temp column to see if there are anomalies
+temp_plot <- ggplot(angr, aes(x=Time, y=Temp_600))
+temp_plot + geom_point()
+
+#transform data to long rather than wide
+
+angr <- angr %>% pivot_longer(-c(Time, Temp_600), names_to = "well_id", values_to = "OD")
+View(test)
+
+#merge plate data with key meta data using inner_join
+angr <- inner_join(angr, angr_key)
+View(angr)
+
+
+#separate out strain, treatment, and replicate into their own columns
+angr <- angr %>% separate(strain_trt_rep_id, into = c("strain", "trt", "rep"), sep = "_", remove = FALSE)
+
+#add "rep_" to values in column "rep"
+angr$rep <- sub("^", "rep_", angr$rep)
+
+
+
